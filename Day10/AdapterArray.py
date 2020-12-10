82622e36f878c1cd0b1350751da06db9546be2c1
ajwhite81 <alex.white81@gmail.com>
2020-12-09 23:29:38 -0800

    day09 update

---
+++ Day10/AdapterArray.py
@@ -0,0 +1,169 @@
+import time
+from typing import List
+from collections import Counter
+
+start = time.time()
+
+TEST1 = """16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4"""
+
+TEST2 = """28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3"""
+
+with open("Day10.txt", 'r') as file:
+    data = file.read()
+
+# class Placeholder:
+#     def __int__(self):
+#         pass
+#
+#     @staticmethod
+#     def parse_line(line: str):
+#         pass
+#
+#
+# TEST_INSTRUCTIONS = [Placeholder.parse_line(line) for line in TEST.strip().splitlines()]
+# INSTRUCTIONS = [Placeholder.parse(line) for line in data.strip().splitlines()]
+
+TEST1_INPUT = [int(x) for x in TEST1.rstrip('\n').strip().splitlines()]
+TEST2_INPUT = [int(x) for x in TEST2.rstrip('\n').strip().splitlines()]
+INPUT = [int(x) for x in data.rstrip('\n').strip().splitlines()]
+
+
+def max_chain(adapters: List[int]) -> int:
+    count1 = 1
+    count3 = 1
+    s = sorted(adapters)
+    pairs = list(zip(s, s[1:]))
+    for pair in pairs:
+        if max(pair) - min(pair) == 1:
+            count1 += 1
+        if max(pair) - min(pair) == 3:
+            count3 += 1
+    return count1 * count3
+
+
+assert max_chain(TEST2_INPUT) == 220
+print(max_chain(INPUT))
+
+"""
+Realize that any time there is a jump of 3 you must pass through that number. The indicates that a logical
+way to break this down is to look at sequences between two jumps of 3
+
+Second you have to realize that there are never any 2 jumps in the actual input. Just ones and threes, which the 
+puzzle doesn't actually say that...so that is annoying.
+
+3 3 -> 1 path
+3 1 3 -> 1 path
+3 1 1 3 -> 2 paths 
+    0 3 4 5 8
+    0 3 5 8
+3 1 1 1 3 -> 4 paths 
+    0 3 4 5 6 9
+    0 3 4 6 9
+    
+    0 3 5 6 9
+    
+    0 3 6 9
+3 1 1 1 1 3 -> 7 paths
+    0 3 4 5 6 7 10
+    
+    0 3 4 6 7 10
+    0 3 4 5 7 10
+    0 3 4 7 10
+    
+    0 3 5 6 7 10
+    0 3 5 7 10
+    
+    0 3 6 7 10
+    
+So count the number of one jumps between two 3 jumps and the pattern ends up being the tribonacci sequence
+https://brilliant.org/wiki/tribonacci-sequence/
+"""
+
+
+def get_diff(adapters: List[int]) -> List[int]:
+    s = sorted(adapters)
+    pairs = list(zip(s, s[1:]))
+    out = []
+    for pair in pairs:
+        out.append(max(pair) - min(pair))
+    return out
+
+
+print(Counter(get_diff(INPUT)))
+
+
+def number_of_chains(adapters: List[int]) -> int:
+    s = sorted(adapters)
+
+    # add in initial 0 to make it work
+    s.insert(0, 0)
+    prev = s[0]
+    one_jumps = 1
+    count = 1
+
+    for n in s[1:]:
+        if n == prev + 1:
+            one_jumps += 1
+        elif one_jumps > 1:
+            count *= tribonacci(one_jumps)
+            one_jumps = 1
+        prev = n
+
+    # end with streak of one jumps
+    if one_jumps > 1:
+        count *= tribonacci(one_jumps)
+
+    return count
+
+
+def tribonacci(n: int, memo=None) -> int:
+    if memo is None:
+        memo = {0: 0, 1: 1, 2: 1}
+    if n not in memo:
+        memo[n] = tribonacci(n - 1, memo) + tribonacci(n - 2, memo) + tribonacci(n - 3, memo)
+    return memo[n]
+
+
+assert number_of_chains(TEST2_INPUT) == 19208
+print(number_of_chains(INPUT))
+print('Time taken {} seconds'.format(round(time.time() - start, 3)))
